4bb5910f9cec3bbe9ba385534b14b3f8d17bfb7d
Kst <andrewsergeev98@gmil.com>
2023-05-15 16:13:39 +0300

    swagger

--- src/main/java/com/generation/application/controller/HelloController.java
+++ src/main/java/com/generation/application/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.generation.application.controller;
-
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @RequestMapping("/api/v1/hello")
+@Tag(name = "Basic", description = "basic method")
 public class HelloController {
-    @GetMapping
+    @GetMapping("/say")
+    @Operation(summary = "new generation")
     public String sayHello() {
         return "Hello from New Generation";
     }
