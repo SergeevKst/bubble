73cd4b677b71ecc2ce9802d2faeecf01e8d68c61
Alexey Paramonov <paramonov_tama@mail.ru>
2023-05-15 18:16:32 +0300

    feat: liquibase changelog

---
+++ src/main/resources/db/changelog/db.changelog-1.0-15.05.sql
@@ -0,0 +1,87 @@
+--liquibase formatted sql
+
+--changeset paramonov:1
+CREATE TABLE IF NOT EXISTS `employee`
+(
+    `id`   INT         NOT NULL AUTO_INCREMENT,
+    `role` VARCHAR(45) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+--rollback DROP TABLE `employee` CASCADE;
+
+--changeset paramonov:2
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id`          INT         NOT NULL AUTO_INCREMENT,
+    `phoneNumber` VARCHAR(45) NOT NULL,
+    `login`       VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `phoneNumber_UNIQUE` (`phoneNumber` ASC) VISIBLE
+);
+--rollback DROP TABLE `users` CASCADE;
+
+--changeset paramonov:3
+CREATE TABLE IF NOT EXISTS `order`
+(
+    `id`           INT  NOT NULL AUTO_INCREMENT,
+    `deliveryDate` DATE NULL DEFAULT NULL,
+    `user_id`      INT  NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_order_user_idx` (`user_id` ASC) VISIBLE,
+    CONSTRAINT `fk_order_user`
+        FOREIGN KEY (`user_id`)
+            REFERENCES `users` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+--rollback DROP TABLE `order` CASCADE;
+
+--changeset paramonov:4
+CREATE TABLE IF NOT EXISTS `orderdetails`
+(
+    `idorder_details` INT         NOT NULL AUTO_INCREMENT,
+    `cost`            INT         NOT NULL,
+    `bubble_count`    INT         NOT NULL,
+    `status`          VARCHAR(45) NULL DEFAULT NULL,
+    `adress`          VARCHAR(45) NULL DEFAULT NULL,
+    `order_id`        INT         NOT NULL,
+    PRIMARY KEY (`idorder_details`),
+    INDEX `fk_orderdetails_order1_idx` (`order_id` ASC) VISIBLE,
+    CONSTRAINT `fk_orderdetails_order1`
+        FOREIGN KEY (`order_id`)
+            REFERENCES `order` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+--rollback DROP TABLE `orderdetails` CASCADE;
+
+--changeset paramonov:5
+CREATE TABLE IF NOT EXISTS `order_has_employee`
+(
+    `order_id`    INT NOT NULL,
+    `employee_id` INT NOT NULL,
+    PRIMARY KEY (`order_id`, `employee_id`),
+    INDEX `fk_order_has_employee_employee1_idx` (`employee_id` ASC) VISIBLE,
+    INDEX `fk_order_has_employee_order1_idx` (`order_id` ASC) VISIBLE,
+    CONSTRAINT `fk_order_has_employee_order1`
+        FOREIGN KEY (`order_id`)
+            REFERENCES `order` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `fk_order_has_employee_employee1`
+        FOREIGN KEY (`employee_id`)
+            REFERENCES `employee` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+);
+--rollback DROP TABLE `order_has_employee` CASCADE;
+
+--changeset paramonov:6
+CREATE TABLE IF NOT EXISTS `storehouse`
+(
+    `id`              INT         NOT NULL,
+    `material_name`   VARCHAR(45) NULL,
+    `amaterial_count` INT         NULL,
+    PRIMARY KEY (`id`)
+)
+--rollback DROP TABLE `storehouse` CASCADE;
