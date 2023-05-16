a1a0f064d7e1df94746975ebf61003596a56ba7a
Kst <andrewsergeev98@gmil.com>
2023-05-16 10:52:30 +0300

    sql, code convention

--- src/main/java/com/generation/application/model/User.java
+++ src/main/java/com/generation/application/model/User.java
@@ -1,7 +1,6 @@
 package com.generation.application.model;
 
 import com.generation.application.role.Role;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -30,8 +29,7 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int idUser;
+    private int id;
     private String phoneNumber;
     private String login;
     @Enumerated(EnumType.STRING)
