e88e9dc173e253c206af1e70c0e3fe9e02e37851
Egor <Sokol66777@gmail.com>
2023-05-25 17:48:38 +0300

    repo

--- src/main/java/com/generation/application/service/ipml/OrderServiceImpl.java
+++ src/main/java/com/generation/application/service/ipml/OrderServiceImpl.java
@@ -4,8 +4,10 @@ import com.generation.application.dto.OrderCreateUpdateDto;
 import com.generation.application.dto.OrderReadDto;
 import com.generation.application.entity.Address;
 import com.generation.application.entity.Order;
+import com.generation.application.entity.User;
 import com.generation.application.mapper.Mapper;
 import com.generation.application.repository.OrderRepository;
+import com.generation.application.repository.UserRepository;
 import com.generation.application.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,9 +21,9 @@ import java.util.Set;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
     private final Mapper<Order,OrderReadDto> orderReadMapper;
-    private final Mapper<OrderCreateUpdateDto,Order> mapperOrderDtoToOrder;
-
+    private final Mapper<OrderCreateUpdateDto,Order> orderFromDtoToEntityMapper;
     @Override
     @Transactional
     public OrderReadDto findByIdWithUser(Integer id) {
@@ -54,10 +56,29 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     @Transactional
-    public void saveOrUpdate(OrderCreateUpdateDto orderCreateUpdateDto) {
-        Order order=mapperOrderDtoToOrder.map(orderCreateUpdateDto);
-        orderRepository.save(order);
+    public Order findById(Integer id) {
+        return orderRepository.findById(id).orElse(null);
     }
 
+    @Override
+    @Transactional
+    public void saveOrder(OrderCreateUpdateDto orderCreateUpdateDto, String login) {
+        Order order = orderFromDtoToEntityMapper.map(orderCreateUpdateDto);
+        User user = userRepository.findByLogin(login).orElse(null);
+        if(user.getOrders()!=null) {
+            user.getOrders().add(order);
+        }else{
+            Set<Order> orderSet = new HashSet<>();
+            orderSet.add(order);
+            user.setOrders(orderSet);
+        }
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(OrderCreateUpdateDto orderCreateUpdateDto) {
+        orderRepository.save(orderFromDtoToEntityMapper.map(orderCreateUpdateDto));
+    }
 
 }
