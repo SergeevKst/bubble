adb56ecccaa475c5de6db4274e296711cec962e5
Alexey Paramonov <paramonov_tama@mail.ru>
2023-06-03 16:03:47 +0300

    feat: получай, Николай, mappers with Mapstruct

---
+++ src/main/java/com/generation/application/mapstructMapper/OrderMapper.java
@@ -0,0 +1,15 @@
+package com.generation.application.mapstructMapper;
+
+import com.generation.application.dto.OrderCreateUpdateDto;
+import com.generation.application.dto.OrderReadDto;
+import com.generation.application.entity.Order;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring",
+        uses = OrderDetailsMapper.class)
+public interface OrderMapper {
+
+    OrderReadDto toDto(Order entity);
+
+    Order toEntity(OrderCreateUpdateDto dto);
+}
